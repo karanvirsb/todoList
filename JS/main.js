dc673d964acf65de8fde4aa495decfa4077b7927
Karavir Boparai <Karanvirboparai123@gmail.com>
2021-06-30 17:57:23 -0400

    created a global event listener that allows easier implementation of event listener for new dom elements

---
+++ JS/main.js
@@ -0,0 +1,8 @@
+
+function addGlobalEventListener(type ,selector, callback){
+    document.addEventListener(type, e =>{
+        if(e.target.matches(selector)){
+            callback(e)
+        }
+    })
+};
