2723896bffbf9b36642e9a0e7ed6c7116a540ec8
Karavir Boparai <Karanvirboparai123@gmail.com>
2021-06-30 20:33:16 -0400

    added some comments for the func addGlobalEventListener and added a function that will delete any item if the trash is clicked

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,8 @@
-
+/** allows you to add event listeners globally event to dynamically added elements
+ * @param  {string} type - type of event listen ex. 'click'
+ * @param  {string} selector - is the element that is being selected ex. '.wrapper'
+ * @param  {anonymous variabl} callback - This is what is being returned the target selected element
+ */
 function addGlobalEventListener(type ,selector, callback){
     document.addEventListener(type, e =>{
         if(e.target.matches(selector)){
@@ -6,3 +10,12 @@ function addGlobalEventListener(type ,selector, callback){
         }
     })
 };
+
+//This section focuses on the deletion of anyhting that has a trash-can to delete
+
+addGlobalEventListener('click', '.trash-can', e=>{
+    const remove_item = e.target.parentElement;
+
+    if(confirm(`Are you sure you want to remove this ${e.target}`))
+        remove_item.remove(); 
+});
