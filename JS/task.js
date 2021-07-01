be00a2b7af51ca73a3311c4d7bd658005fd838a0
Karavir Boparai <Karanvirboparai123@gmail.com>
2021-06-30 22:40:51 -0400

    added the restore feature with minor bugs and added a moveTo function that moves tasks from one section to the other

--- JS/task.js
+++ JS/task.js
@@ -1,9 +1,14 @@
-let task; 
+const completed_task_template = document.querySelector('.completed-template').content.children[0];
+const task_template = document.querySelector('.task-template').content.children[0];
+const completedTask = document.querySelector('#completed-tasks');
+const tasks = document.querySelector('#tasks'); 
+const task_parent_id = 'tasks'; 
+const completed_parent_id = 'completed-tasks';
 
 // finds the parent of the edit task and allows editing of the task 
 addGlobalEventListener('click', '.edit-task', e=>{
-    task = e.target.parentElement;
-    const taskInfo = task.children[1];
+    const task = e.target.parentElement;
+    const taskInfo = task.querySelector('.task-information');
     taskInfo.setAttribute('contenteditable', 'true');
     taskInfo.focus();   
     console.log(taskInfo);
@@ -21,3 +26,54 @@ addGlobalEventListener('focusout', '.task-information', e =>{
     e.target.setAttribute('contenteditable', 'false'); 
 })
 
+// this is working on moving the task to the completed section 
+
+addGlobalEventListener('click', '.task-checkboxes', e=>{
+    const task = e.target.parentElement;  
+    const taskInfo  = task.querySelector('.task-information').innerHTML.trim();
+    
+    if(task.parentElement.id === task_parent_id){
+        moveTo(taskInfo, completed_parent_id);
+        task.remove();
+    }
+})
+
+
+// This will restore the task back upto tasks from completed
+
+addGlobalEventListener('click', '.restore-task', e => {
+    const task = e.target.parentElement; 
+    const taskInfo = task.querySelector('.task-information').innerHTML.trim();
+    moveTo(taskInfo , task_parent_id); 
+    setTimeout(completedTask.removeChild(task), 10000);
+    
+});
+
+
+function moveTo(taskDetails, section){
+
+    console.log(completed_task_template);
+    console.log(task_template);
+
+    const completeTemp = completed_task_template;
+    const taskTemp = task_template;
+    
+    console.log(completeTemp);
+    console.log(taskTemp);
+
+    switch(section){
+        case completed_parent_id: 
+            completeTemp.querySelector('.task-information').innerHTML = taskDetails; 
+            completedTask.appendChild(completeTemp);
+            break;
+        case task_parent_id:
+            taskTemp.querySelector('.task-information').innerHTML = taskDetails;
+            taskTemp.querySelector('.task-checkboxes').removeAttribute('checked'); 
+            tasks.appendChild(taskTemp);
+            break; 
+        default:
+            console.log(`There is no section named ${section}`)
+            break; 
+    }
+   
+}
