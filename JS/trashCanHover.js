893f9797a5ea193afc3f3d619fee90638d892934
Karavir Boparai <Karanvirboparai123@gmail.com>
2021-07-02 21:41:54 -0400

    removed whitespace

--- JS/trashCanHover.js
+++ JS/trashCanHover.js
@@ -3,13 +3,6 @@ addGlobalEventListener('mouseover', '.trash-can', e => {
     e.target.setAttribute('src','../Assets/open-trash-can.png'); 
 });
 
-
 addGlobalEventListener('mouseout', '.trash-can', e =>{
     e.target.setAttribute('src', '../Assets/trash-can.png');
 });
-
-
-
-
-
-
