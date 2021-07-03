9d50282a9f44b3a97fab405b15089b087872ff75
Karavir Boparai <Karanvirboparai123@gmail.com>
2021-07-02 21:39:39 -0400

    removed a variable active and added a function activeCategory

--- JS/category.js
+++ JS/category.js
@@ -1,9 +1,8 @@
 const category_item = document.querySelector('.category-item'); 
-const classActive = 'active';
 const category_list_container = document.querySelector('#category-list');
 
 category_list_container.addEventListener('click', e=>{
     if(e.target.tagName.toLowerCase() === 'li'){
-        e.target.classList.add(classActive); 
+        activeCategory(e.target.dataset.categoryId); 
     }
 });
