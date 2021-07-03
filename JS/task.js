a4d77801384b81ba20c799f6d2af75829fd20856
Karavir Boparai <Karanvirboparai123@gmail.com>
2021-07-03 16:13:30 -0400

    removed some variables into main. also removed the restore event. and added some save and render functions

--- JS/task.js
+++ JS/task.js
@@ -1,8 +1,4 @@
-const completedTask = document.querySelector('#completed-tasks');
-const tasks = document.querySelector('#tasks'); 
 
-const task_parent_id = 'tasks'; 
-const completed_parent_id = 'completed-tasks';
 
 // finds the parent of the edit task and allows editing of the task 
 addGlobalEventListener('click', '.edit-task', e=>{
@@ -10,38 +6,38 @@ addGlobalEventListener('click', '.edit-task', e=>{
     const taskInfo = task.querySelector('.task-information');
     taskInfo.setAttribute('contenteditable', 'true');
     taskInfo.focus();   
-    console.log(taskInfo);
 });
 
 // if the user presses enter then the task is not editable and the user has entered what they wanted to
 addGlobalEventListener('keydown', '.task-information', e=>{
     if(e.code === 'Enter'){
-        e.target.setAttribute('contenteditable', 'false'); 
+        const task = e.target.parentElement;
+        const taskInfo = task.querySelector('.task-information').innerHTML;
+        e.target.setAttribute('contenteditable', 'false');
+        updateTask(taskInfo, task.dataset.taskId);
+        saveAndRender(); 
     }
 })
 
 // If the user clicks off then the task is complete and not editable unless clicking the button 
 addGlobalEventListener('focusout', '.task-information', e =>{
+    const task = e.target.parentElement;
+    const taskInfo = task.querySelector('.task-information').innerHTML;
+
     e.target.setAttribute('contenteditable', 'false'); 
+    updateTask(taskInfo, task.dataset.taskId);
+    saveAndRender();
+    renderCounts(); 
 })
 
 // this is working on moving the task to the completed section 
 
 addGlobalEventListener('click', '.task-checkboxes', e=>{
     const task = e.target.parentElement;
-    // task.classList.add('')   
-    // completedTask.appendChild(task); 
-    
-    // if(task.parentElement.id === task_parent_id){
-    //     tasks.removeChild(task); 
-    //     renderCounts(); 
-    // }
-
-    const taskInfo  = task.querySelector('.task-information').innerHTML.trim();
     
     if(task.parentElement.id === task_parent_id){
-        createCategory(taskInfo);
-        tasks.removeChild(task);
+        createCompletedTask(task.dataset.taskId);
+        saveAndRender();
         renderCounts();
     }
 })
@@ -49,13 +45,13 @@ addGlobalEventListener('click', '.task-checkboxes', e=>{
 
 // This will restore the task back upto tasks from completed
 
-addGlobalEventListener('click', '.restore-task', e => {
-    const task = e.target.parentElement; 
-    const taskInfo = task.querySelector('.task-information').innerHTML.trim();
+// addGlobalEventListener('click', '.restore-task', e => {
+//     const task = e.target.parentElement; 
+//     const taskInfo = task.querySelector('.task-information').innerHTML.trim();
 
-    createTask(taskInfo); 
-    completedTask.removeChild(task);
-    renderCounts();
-});
+//     createTask(taskInfo); 
+//     completedTask.removeChild(task);
+//     renderCounts();
+// });
 
 
