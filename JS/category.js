d187966d22bcb6a5123e8a19f2f5a38f4130959f
Karavir Boparai <Karanvirboparai123@gmail.com>
2021-07-03 16:11:47 -0400

    changed up the category click detects even if you clicked the p tag

--- JS/category.js
+++ JS/category.js
@@ -2,7 +2,16 @@ const category_item = document.querySelector('.category-item');
 const category_list_container = document.querySelector('#category-list');
 
 category_list_container.addEventListener('click', e=>{
-    if(e.target.tagName.toLowerCase() === 'li'){
-        activeCategory(e.target.dataset.categoryId); 
+    const element = e.target; 
+    const elementTag = element.tagName.toLowerCase();
+    switch(elementTag){
+        case 'li':
+            activeCategory(element.dataset.categoryId);
+            renderCounts(); 
+            break;
+        case 'p':
+            activeCategory(element.parentElement.dataset.categoryId);
+            renderCounts(); 
+            break; 
     }
 });
